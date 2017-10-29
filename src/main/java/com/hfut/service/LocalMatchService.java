d17c8bc3e79428ad82601fef4d145c9ed3790d18
amoxu <amoxu@qq.com>
2017-10-29 14:43:17 +0800

    fix all

---
+++ src/main/java/com/hfut/service/LocalMatchService.java
@@ -0,0 +1,20 @@
+package com.hfut.service;
+
+import com.hfut.entity.LMatchWorkload;
+
+import java.util.List;
+
+public interface LocalMatchService {
+    List<LMatchWorkload> findAllLoad(Integer page, Integer limit) throws Exception;
+
+    int getCount() throws Exception;
+
+    boolean updateLoad(LMatchWorkload workload) throws Exception;
+
+    boolean removeLoad(Integer id) throws Exception;
+
+    boolean insertLoad(LMatchWorkload workload) throws Exception;
+
+    List<LMatchWorkload> insertLoadByList(List<LMatchWorkload> list) throws Exception;
+
+}
---
+++ src/main/java/com/hfut/service/impl/LocalMatchServiceImpl.java
@@ -0,0 +1,81 @@
+package com.hfut.service.impl;
+
+import com.hfut.entity.LMatchWorkload;
+import com.hfut.entity.LMatchWorkloadExample;
+import com.hfut.mapper.LMatchWorkloadMapper;
+import com.hfut.service.LocalMatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class LocalMatchServiceImpl implements LocalMatchService {
+    @Autowired
+    private LMatchWorkloadMapper workloadMapper;
+
+    @Override
+    public List<LMatchWorkload> findAllLoad(Integer page, Integer limit) throws Exception {
+        LMatchWorkloadExample example = new LMatchWorkloadExample();
+        LMatchWorkloadExample.Criteria criteria = example.createCriteria();
+        criteria.andIdIsNotNull();
+        example.setOffset(page);
+        example.setLimit(limit);
+        return workloadMapper.selectByExample(example);
+    }
+
+    @Override
+    public int getCount() throws Exception {
+        LMatchWorkloadExample example = new LMatchWorkloadExample();
+        LMatchWorkloadExample.Criteria criteria = example.createCriteria();
+        criteria.andIdIsNotNull();
+        return workloadMapper.countByExample(example);
+    }
+
+    @Override
+    public boolean updateLoad(LMatchWorkload workload) throws Exception {
+        //计算工作量 课时
+
+        LMatchWorkloadExample workloadExample = new LMatchWorkloadExample();
+        LMatchWorkloadExample.Criteria criteria = workloadExample.createCriteria();
+        criteria.andIdEqualTo(workload.getId());
+        if (workloadMapper.updateByExample(workload, workloadExample) == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public boolean removeLoad(Integer id) throws Exception {
+        LMatchWorkloadExample expWorkloadExample = new LMatchWorkloadExample();
+        LMatchWorkloadExample.Criteria criteria = expWorkloadExample.createCriteria();
+        criteria.andIdEqualTo(id);
+        if (workloadMapper.deleteByExample(expWorkloadExample) == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public boolean insertLoad(LMatchWorkload workload) throws Exception {
+
+        if (workloadMapper.insertSelective(workload) == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<LMatchWorkload> insertLoadByList(List<LMatchWorkload> list) throws Exception {
+        ArrayList<LMatchWorkload> ret = new ArrayList<LMatchWorkload>();
+        for (LMatchWorkload workload : list) {
+            if (!insertLoad(workload)) {
+                ret.add(workload);
+            }
+        }
+        return ret;
+    }
+
+}
