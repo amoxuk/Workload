e98f1397b71a1b61fe10145e683a44a1695b00d6
amoxu <amoxu@qq.com>
2017-11-26 23:27:02 +0800

    精简了一些冗余的内容

--- src/main/webapp/js/setting.js
+++ src/main/webapp/js/setting.js
@@ -37,7 +37,8 @@
     }
     }
     }
-    window.onload = function(){
+
+    window.onload = function () {
         document.getElementsByTagName("footer")[0].innerHTML = "©CopyRight 2017 合肥工业大学 All Rights Reserved.";
 
         var obj = "  <ul class=\"layui-nav\" lay-filter='demo'> \n" +
@@ -68,14 +69,14 @@
             "                       <dd><a href=\"../workload/netCourse.html\">网络课程</a></dd>\n" +
             "                       <dd><a href=\"../workload/project.html\">双创项目</a></dd>\n" +
             "                       <dd><a href=\"../workload/competition.html\">竞赛</a></dd>\n" +
-    /*        "                       <dd><a href=\"../workload/publishTextbook.html\">教材编写</a></dd>\n" +
-            "                       <dd><a href=\"../workload/trainAxe.html\">乐器指导</a></dd>\n" +*/
+            /*        "                       <dd><a href=\"../workload/publishTextbook.html\">教材编写</a></dd>\n" +
+                    "                       <dd><a href=\"../workload/trainAxe.html\">乐器指导</a></dd>\n" +*/
             "                       </dl>\n" +
             "               </li>\n" +
             "           <li class=\"layui-nav-item\">\n" +
-            "                   <a href=\"javascript:;\">实验教学</a>\n" +
+            "                   <a href=\"../workload/exp.html\">实验教学</a>\n" +
             "               </li>\n" +
-            "           <li style=\"float: right\" class=\"layui-nav-item \"><a href=\"/../../userlogin.html\">退出</a></li>\n" +
+            "           <li style=\"float: right\" class=\"layui-nav-item \"><a href=\"/logout\">退出</a></li>\n" +
             "       </ul>\n" +
             "       <script>\n" +
             "           layui.use('element', function(){\n" +
@@ -84,23 +85,22 @@
             "               //监听导航点击\n" +
             "               element.on('nav(demo)', function(elem){\n" +
             "                   //console.log(elem)\n" +
-/*            "                   layer.msg(elem.text());\n" +*/
+            /*            "                   layer.msg(elem.text());\n" +*/
             "               });\n" +
-            "           });\n" +
-            "       </script>\n" +
-            "        <br>";  //按个人要求拼接标签
+            "           });" +
+            "       </script>";//按个人要求拼接标签
         $("header").append(obj);  // 用append 方式添加拼接的标签
 
         $("header").trigger("create");  //重点：这名的意思是重新加载所在标签的样式。非常有用的一句话，不加这一句你动态append的标签将丢失Css样式
 
-        document.getElementsByTagName("main")[0] = document.getElementsByTagName("main")[0].innerHTML+"<br><br><br>";
-        layui.use('element', function(){
+        document.getElementsByTagName("main")[0] = document.getElementsByTagName("main")[0].innerHTML + "<br><br><br>";
+        layui.use('element', function () {
             var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
 
             //监听导航点击
-            element.on('nav(demo)', function(elem){
+            element.on('nav(demo)', function (elem) {
                 //console.log(elem)
                 //layer.msg(elem.text());
             });
         });
-    }
+    };
